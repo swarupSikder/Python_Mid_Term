0cc8bb856735d8a53bffee89ad99f510ff86296a
swarupSikder <swarupfx20@gmail.com>
2025-04-11 21:16:16 +0600

    Data Privacy

--- Student_Management.py
+++ Student_Management.py
@@ -1,7 +1,7 @@
 class Student:
     def __init__(self, student_id, name, department, is_enrolled):
         self.__student_id = student_id
-        self.name = name
+        self.__name = name
         self.__department = department
         self.__is_enrolled = is_enrolled
 
@@ -12,11 +12,23 @@ class Student:
             database = StudentDatabase()
             database.student_list.append(student)
         else:
-            print(f'Trying to enroll {self.name} who is already enrolled')
+            print(f'Trying to enroll {self.get_name()} who is already enrolled')
 
     # To Access the private id
     def get_id(self):
         return self.__student_id
+    
+    # To Access the private name
+    def get_name(self):
+        return self.__name
+    
+    # To Access the private department
+    def get_dept(self):
+        return self.__department
+    
+    # To Access the private enrolled
+    def get_enrolled(self):
+        return self.__is_enrolled
 
     def drop_student(self):
         if self.__is_enrolled == True:
@@ -24,22 +36,22 @@ class Student:
             
             # modify the is_enrolled to false for student
         else:
-            print(f'Trying to drop {self.name} who is not enrolled')
+            print(f'Trying to drop {self.get_name()} who is not enrolled')
 
 
     def view_student_info(self):
-        print(f'ID: {self.__student_id}, Name: {self.name}, Dept: {self.__department}, Enrolled: {self.__is_enrolled}')
+        print(f'ID: {self.get_id()}, Name: {self.get_name()}, Dept: {self.get_dept()}, Enrolled: {self.get_enrolled()}')
 
 
     def __repr__(self):
-        return f'ID: {self.__student_id}, Name: {self.name}, Dept: {self.__department}, Enrolled: {self.__is_enrolled}'
+        return f'ID: {self.get_id()}, Name: {self.get_name()}, Dept: {self.get_dept()}, Enrolled: {self.get_enrolled()}'
     
 
 class StudentDatabase:
     student_list = [
-        Student(100, 'ALif', 'IPE', True),
+        Student(100, 'Alif', 'IPE', True),
         Student(101, 'Kopa', 'CSE', True),
-        Student(102, '', 'EEE', False),
+        Student(102, 'Samsu', 'EEE', False),
         Student(103, 'Sikder', 'CSE', True)
     ]
 
@@ -66,7 +78,8 @@ class StudentDatabase:
 
 famousSchool = StudentDatabase()
 while True:
-    print('\n1. View All Students')
+    print('\n----Student Management Menu----')
+    print('1. View All Students')
     print('2. Enroll Student')
     print('3. Drop Student')
     print('4. Exit')
@@ -75,14 +88,21 @@ while True:
     if opt == '1':
         famousSchool.view_all_students()
     elif opt == '2':
-        newStudent = Student(103, 'Alan', 'EEE', True)
+        st_id = int(input('Enter a valid Student ID: '))
+        st_name = input('Enter Student Name: ')
+        st_dept = input('Enter department: ')
+        st_enrolled = input('Enter enrollment status: ')
+        newStudent = Student(st_id, st_name, st_dept, st_enrolled)
+
         if newStudent.get_id() > 99 and newStudent.get_id() < 201:
             newStudent.enroll_student(newStudent)
             famousSchool.view_all_students()
         else:
             print('Invalid student ID')
     elif opt == '3':
-        student = famousSchool.get_student_by_ID(103)
+        st_id = int(input('Enter a valid Student ID: '))
+
+        student = famousSchool.get_student_by_ID(st_id)
         if student and newStudent.get_id() > 99 and newStudent.get_id() < 201:
             student.drop_student()
             famousSchool.view_all_students()
