6379c9372c8985e2c3b8e23e11795fb09b72d7bb
swarupSikder <swarupfx20@gmail.com>
2025-04-11 21:14:30 +0600

    Implement enroll_student() method

--- Student_Management.py
+++ Student_Management.py
@@ -1,30 +1,42 @@
 class Student:
     def __init__(self, student_id, name, department, is_enrolled):
-        self.student_id = student_id
+        self.__student_id = student_id
         self.name = name
-        self.department = department
-        self.is_enrolled = is_enrolled
+        self.__department = department
+        self.__is_enrolled = is_enrolled
+
+    def enroll_student(self, student):
+        if self.__is_enrolled == False:
+            self.__is_enrolled = True           # it will change id:104 (False -> True)
+            
+            database = StudentDatabase()
+            database.student_list.append(student)
+        else:
+            print(f"{self.name} is already enrolled.")
 
     def __repr__(self):
-        return f'ID: {self.student_id}, Name: {self.name}, Dept: {self.department}, Enrolled: {self.is_enrolled}'
+        return f'ID: {self.__student_id}, Name: {self.name}, Dept: {self.__department}, Enrolled: {self.__is_enrolled}'
     
 
 class StudentDatabase:
-    student_list = []
+    student_list = [
+        Student(100, 'ALif', 'IPE', True),
+        Student(101, 'Kopa', 'CSE', True),
+        Student(102, '', 'EEE', False),
+        Student(103, 'Sikder', 'CSE', True)
+    ]
 
-    def add_student(self, student):
-        self.student_list.append(student)
-        
+
+    
+
+    
+
 
 famousSchool = StudentDatabase()
-student_1 = Student(101, 'Kopa', 'CSE', True)
-student_2 = Student(102, 'Samsu', 'EEE', False)
-
-famousSchool.add_student(student_1)
-famousSchool.add_student(student_2)
+s1 = Student(104, 'Alan', 'EEE', False)
+s1.enroll_student(s1)
 
 
-
-# output
+# # output
 for student in famousSchool.student_list:
     print(student)
