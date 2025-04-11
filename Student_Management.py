543f4b9861cff3b5c193549a62cd2904a1bc9f7b
swarupSikder <swarupfx20@gmail.com>
2025-04-11 21:13:33 +0600

    Create the Student class

--- Student_Management.py
+++ Student_Management.py
@@ -1,28 +1,17 @@
 class Student:
-    def __init__(self, name, roll, result):
+    def __init__(self, student_id, name, department, is_enrolled):
+        self.student_id = student_id
         self.name = name
-        self.roll = roll
-        self.result = result
+        self.department = department
+        self.is_enrolled = is_enrolled
 
     def __repr__(self):
-        return f'Name: {self.name} | Roll: {self.roll} | Result: {self.result}'
-
-class StudentDatabase:
-    student_list = []
-
-    @classmethod
-    def add_student(cls, name, roll, result):
-        student = Student(name, roll, result)
-        cls.student_list.append(student)
-
-StudentDatabase.add_student('koba', 10, 'A+')
-StudentDatabase.add_student('sik', 9, 'A+')
-StudentDatabase.add_student('der', 10, 'B+')
-StudentDatabase.add_student('swarup', 11, 'A')
-StudentDatabase.add_student('Ritu', 10, 'A-')
+        return f'ID: {self.student_id}, Name: {self.name}, Dept: {self.department}, Enrolled: {self.is_enrolled}'
+    
 
 
+student_1 = Student(101, 'Kopa', 'CSE', True)
+student_2 = Student(102, 'Samsu', 'EEE', False)
 
-# Output
-for student in StudentDatabase.student_list:
-    print(student)
+print(student_1)
+print(student_2)
